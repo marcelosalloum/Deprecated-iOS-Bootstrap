5e6793c38dee7d686cf31a512497c712f45bf761
Marcelo Salloum dos Santos <marcelosalloum@gmail.com>
2019-02-05 17:48:47 -0200

    Improved Login Screen

--- marcelo-feedback-loop/iOS Bootstrap/Screens/Auth/Login/LoginViewController.swift
+++ marcelo-feedback-loop/iOS Bootstrap/Screens/Auth/Login/LoginViewController.swift
@@ -11,12 +11,19 @@ import UIKit
 class LoginViewController: CoordinatedViewController {
 
     weak var coordinator: LoginViewControllerDelegate?
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordFied: UITextField!
+
+    @IBAction func loginButtonClicked(_ sender: Any) {
+        self.toastr("loginButtonClicked with -mail: \(emailField.text!), password: \(passwordFied.text!)")
+    }
 
     @IBAction func forgotPasswordClicked(_ sender: UIButton) {
         coordinator?.userDidClickForgotPassword()
     }
 
     override func viewWillAppear(_ animated: Bool) {
+        navigationController?.navigationBar.prefersLargeTitles = false
         navigationController?.setNavigationBarHidden(false, animated: true)
         super.viewWillAppear(animated)
     }
